e44bf2f043c96488dd9b6b19fcad7ed45793e743
Phi Hung <phnguyen.cs@gmail.com>
2026-07-09 00:45:05 +0700

    deploy ec2

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
         host: '0.0.0.0',
         port: 5173,
         proxy: {
-            '/api': 'http://127.0.0.1:8000',
+            '/api': 'http://13.229.132.48',
         },
     },
 });
